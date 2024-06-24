05fbd6c8cd21cf416b601de1ed90e1333dacd9ad
vfreis09 <reis.vicente27@gmail.com>
2024-06-24 15:45:31 -0300

    auhtorization middleware in backend implemented

--- server/src/middlewares/middleware.ts
+++ server/src/middlewares/middleware.ts
@@ -1,3 +1,5 @@
+import { Request, Response, NextFunction } from "express";
+import { pool } from "../config/dbConfig";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -85,7 +87,36 @@ async function getGoogleUser({
   }
 }
 
+const isAuthorized = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const postId = req.params.id; // Assuming the post ID is passed in the request params
+  const userId = req.session.user.user_id; // Assuming you have the user ID in the session
+
+  try {
+    const post = await pool.query("SELECT _id FROM posts WHERE id = $1", [
+      postId,
+    ]);
+
+    if (post.rows.length === 0) {
+      return res.status(404).send("Post not found");
+    }
+
+    if (post.rows[0].user_id !== userId) {
+      return res.status(403).send("Unauthorized");
+    }
+
+    next(); // User is authorized, proceed to the next middleware
+  } catch (error) {
+    console.error("Error checking authorization:", error);
+    res.status(500).send("Server error");
+  }
+};
+
 const middlewares = {
+  isAuthorized,
   getGoogleOauthTokens,
   getGoogleUser,
 };
--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import postController from "../controllers/postController";
+import middlewares from "../middlewares/middleware";
 
 const router = Router();
 
 router.post("/", postController.createPost);
 router.get("/", postController.getPosts);
 router.get("/:id", postController.getPostById);
-router.put("/:id", postController.updatePost);
-router.delete("/:id", postController.deletePost);
+router.put("/:id", middlewares.isAuthorized, postController.updatePost);
+router.delete("/:id", middlewares.isAuthorized, postController.deletePost);
 
 export default router;
