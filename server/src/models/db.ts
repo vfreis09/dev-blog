4b76213728eaaf080f1bd0e7d031413871c6022a
vfreis09 <reis.vicente27@gmail.com>
2024-07-01 14:02:55 -0300

    create table likes and comments

--- server/src/models/db.ts
+++ server/src/models/db.ts
@@ -7,15 +7,28 @@ const initDb = async () => {
     google_id VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    picture VARCHAR(255)
-);
+    picture VARCHAR(255));
 
   CREATE TABLE IF NOT EXISTS posts (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   content TEXT NOT NULL,
   author_id INTEGER REFERENCES users(id),
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)`;
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+  
+  CREATE TABLE IF NOT EXISTS likes (
+  id SERIAL PRIMARY KEY,
+  post_id INT REFERENCES posts(id),
+  author_id INTEGER REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+
+CREATE TABLE IF NOT EXISTS comments (
+  id SERIAL PRIMARY KEY,
+  post_id INT REFERENCES posts(id),
+  author_id INTEGER REFERENCES users(id),
+  content TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+  `;
 
   try {
     await pool.query(createTableQuery);
