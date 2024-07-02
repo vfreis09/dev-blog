aceb3c07b3d5c0fd5af729489c07aba5b1967b3c
vfreis09 <reis.vicente27@gmail.com>
2024-07-02 13:42:40 -0300

    cookie session time increased

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,11 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const maxAge = 1000 * 60 * 60 * 24;
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge },
   })
 );
 
