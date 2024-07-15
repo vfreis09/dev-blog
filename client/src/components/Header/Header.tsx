7520d4293621beb7c9f234198101dbc26b0f2bbd
vfreis09 <reis.vicente27@gmail.com>
2024-07-15 14:34:26 -0300

    add avatar user pic and logout colapse div

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import getGoogleOauthUrl from "../../utils/getGoogleUrl";
 import { useUser } from "../../context/UserContext";
-import Button from "react-bootstrap/Button";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
+import {
+  Button,
+  Navbar,
+  Nav,
+  Container,
+  Image,
+  Collapse,
+} from "react-bootstrap";
+import styles from "./Header.module.css";
 
 const Header: React.FC = () => {
   const { user, isLoggedIn, setUser, setIsLoggedIn } = useUser();
+  const [open, setOpen] = useState(false);
 
   const handleLogout = async () => {
     try {
@@ -33,42 +39,67 @@ const Header: React.FC = () => {
   };
 
   return (
-    <Navbar sticky="top" bg="primary" data-bs-theme="dark" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          dev-blog
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {isLoggedIn && (
-              <Nav.Link as={Link} to="/create">
-                Create Post
-              </Nav.Link>
-            )}
-          </Nav>
-          <Nav>
-            {isLoggedIn ? (
-              <>
-                <Nav.Link disabled>Welcome, {user?.name}</Nav.Link>
-                <Button variant="outline-light" onClick={handleLogout}>
-                  Logout
+    <>
+      <Navbar sticky="top" bg="primary" data-bs-theme="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/" className={styles.navBrand}>
+            dev-blog
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {isLoggedIn && (
+                <Nav.Link as={Link} to="/create">
+                  Create Post
+                </Nav.Link>
+              )}
+            </Nav>
+            <Nav>
+              {isLoggedIn ? (
+                <>
+                  <Image
+                    src={user?.picture}
+                    roundedCircle
+                    height="40"
+                    width="40"
+                    alt="User Avatar"
+                    onClick={() => setOpen(!open)}
+                    style={{ cursor: "pointer" }}
+                  />
+                </>
+              ) : (
+                <Button variant="success">
+                  <Link
+                    to={getGoogleOauthUrl()}
+                    style={{ textDecoration: "none", color: "inherit" }}
+                  >
+                    Login
+                  </Link>
                 </Button>
-              </>
-            ) : (
-              <Button variant="success">
-                <Link
-                  to={getGoogleOauthUrl()}
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  Login
-                </Link>
-              </Button>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Collapse in={open}>
+        <div className={styles.collapseContainer}>
+          <div className="bg-light p-3 rounded">
+            <Image
+              src={user?.picture}
+              roundedCircle
+              height="40"
+              width="40"
+              alt="User Avatar"
+            />
+            <p>{user?.name}</p>
+            <p>{user?.email}</p>
+            <Button variant="danger" onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        </div>
+      </Collapse>
+    </>
   );
 };
 
